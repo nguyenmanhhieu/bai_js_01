3e771e0030aaf79e9bfc9c20f2ccd56f5e06a637
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-07-26 21:27:35 +0700

    bai code vi du JS
    Nguyen Manh Hieu

---
+++ c02/js/update-array.js
@@ -0,0 +1,4 @@
+var color = ['white','black','custom'];
+color[2]='custom';
+var el = document.getElementById('colors');
+el.textContent=color[2];
---
+++ c02/js/update-variable.js
@@ -0,0 +1,12 @@
+var username;
+var messenge;
+
+username = 'Molly';
+messenge = 'See our upcoming range';
+
+var elName = document.getElementById('stock');
+elName.textContent=username;
+
+var elNote = document.getElementById('shipping');
+
+elNote.textContent=messenge;
